2770a35ddb9162dd1b49235f061877183721ec8e
piny940 <an.s.shoma@gmail.com>
2023-12-09 14:47:44 +0900

    image updateを検知するようfluxの設定を更新

--- kubernetes/flux-system/init.sh
+++ kubernetes/flux-system/init.sh
@@ -1,7 +1,10 @@
+kubectl -n flux-system delete secret flux-system
 flux bootstrap github \
+  --components-extra=image-reflector-controller,image-automation-controller \
   --token-auth \
   --owner=piny940 \
   --repository=infra \
   --branch=main \
   --path=kubernetes \
+  --read-write-key \
   --personal
