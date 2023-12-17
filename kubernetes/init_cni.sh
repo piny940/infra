6fe425924ca873748a1475b2dba26ab752b2c6f4
piny940 <an.s.shoma@gmail.com>
2023-12-17 11:41:22 +0900

    cniをflannelに変更

--- kubernetes/init_cni.sh
+++ kubernetes/init_cni.sh
@@ -1,1 +1,2 @@
-kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+# kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
