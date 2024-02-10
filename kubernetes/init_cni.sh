1a279f182f238aa42f9e57485638e4e4b3cfc7fe
piny940 <an.s.shoma@gmail.com>
2024-02-10 10:27:50 +0900

    flannelをkustomizationでインストールするようにする

--- kubernetes/init_cni.sh
+++
@@ -1,2 +0,0 @@
-# kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
-kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
