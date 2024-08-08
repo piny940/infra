02bda5edd77f7a702f158f18e6822229d5a953a8
piny940 <83708535+piny940@users.noreply.github.com>
2024-08-08 22:17:12 +0900

    helmReleaseのalertを自動で作成するスクリプトを作成

---
+++ kubernetes/scripts/flux-helm-alert.sh
@@ -0,0 +1,29 @@
+kustomization() {
+  env=$1
+
+  echo "apiVersion: notification.toolkit.fluxcd.io/v1beta3
+kind: Alert
+metadata:
+  name: helm-alert
+  namespace: flux-system
+spec:
+  summary: \"Helm alert\"
+  eventMetadata:
+    env: ${env}
+    cluster: \"home-cluster\"
+    region: \"kyoto/japan\"
+  providerRef:
+    name: slack-bot
+  eventSeverity: info
+  eventSources:"
+}
+
+for env in 'staging'; do
+  path=apps/flux-alerts/staging/helm-alert.yaml
+  kustomization ${env} > ${path}
+  for ns in `cat namespaces/namespaces.yaml | grep name: | cut -c 9-`; do
+    echo "    - kind: HelmRelease
+      name: \"*\"
+      namespace: ${ns}" >> ${path}
+  done
+done
