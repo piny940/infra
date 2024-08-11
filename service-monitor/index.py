c5d25c96748efa5f82debe2cc1c945430a83f7e4
piny940 <83708535+piny940@users.noreply.github.com>
2024-08-11 11:46:27 +0900

    exit関数を使わないようにする

--- service-monitor/index.py
+++ service-monitor/index.py
@@ -4,6 +4,21 @@ from slack_sdk import WebClient
 import sys
 import dotenv
 
+
+def notify_slack():
+  slack_token = os.getenv('SLACK_API_TOKEN')
+  if slack_token == None:
+    print('SLACK_API_TOKEN environment variable is not set')
+    exit(1)
+  client = WebClient(token=slack_token)
+
+  channel = os.getenv('SLACK_CHANNEL')
+  if channel == None:
+    print('SLACK_CHANNEL environment variable is not set')
+    exit(1)
+  client.chat_postMessage(channel=channel, text='Alert Manager is down! :fire:')
+
+
 if '--dev' in sys.argv:
   dotenv.load_dotenv(dotenv.find_dotenv('.env'))
 
@@ -14,18 +29,6 @@ if path == None:
 print('Checking health of service...')
 res = requests.get(path)
 
-if res.status_code == 200:
-  print('Service is healthy')
-  exit(0)
-
-slack_token = os.getenv('SLACK_API_TOKEN')
-if slack_token == None:
-  print('SLACK_API_TOKEN environment variable is not set')
-  exit(1)
-client = WebClient(token=slack_token)
-
-channel = os.getenv('SLACK_CHANNEL')
-if channel == None:
-  print('SLACK_CHANNEL environment variable is not set')
-  exit(1)
-client.chat_postMessage(channel=channel, text='Alert Manager is down! :fire:')
+if res.status_code != 200:
+  notify_slack()
+  print('Service is down. Alert sent to slack')
