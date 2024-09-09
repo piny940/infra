87953b2b44ba1a9768ba7aceee5626eb27baa13b
piny940 <83708535+piny940@users.noreply.github.com>
2024-09-09 12:32:42 +0900

    backendにgcsを指定

---
+++ gcp/staging/backend.tf
@@ -0,0 +1,5 @@
+terraform {
+  backend "gcs" {
+    bucket = "209cf6053e8989d0-terraform-remote-backend"
+  }
+}
--- gcp/staging/main.tf
+++ gcp/staging/main.tf
@@ -5,3 +5,29 @@ provider "google" {
     environment = "staging"
   }
 }
+
+resource "random_id" "terraform_remote_backend" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "terraform_remote_backend" {
+  name     = "${random_id.terraform_remote_backend.hex}-terraform-remote-backend"
+  location = "US"
+
+  force_destroy               = false
+  public_access_prevention    = "enforced"
+  uniform_bucket_level_access = true
+}
+
+resource "local_file" "default" {
+  file_permission = "0644"
+  filename        = "${path.module}/backend.tf"
+
+  content = <<-EOT
+  terraform {
+    backend "gcs" {
+      bucket = "${google_storage_bucket.terraform_remote_backend.name}"
+    }
+  }
+  EOT
+}
