ebf42d7622003901c769c64a64fc509955d53c5c
piny940 <83708535+piny940@users.noreply.github.com>
2024-09-09 12:48:35 +0900

    gcsのbacket名を修正

--- gcp/staging/gcs.tf
+++ gcp/staging/gcs.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "clubroom" {
-  name     = "com.piny940.clubroom"
+  name     = "clubroom.piny940.com"
   location = "asia-southeast1"
 
   public_access_prevention    = "inherited"
