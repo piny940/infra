7af6a21ff8bef3eb3248f256adaec046d93a7286
piny940 <83708535+piny940@users.noreply.github.com>
2024-09-09 14:13:05 +0900

    productionのterraformをセットアップ

---
+++ gcp/production/main.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  project = "prd-piny940"
+  default_labels = {
+    service     = "home-cluster"
+    environment = "production"
+  }
+}
+
+resource "google_storage_bucket" "terraform_remote_backend" {
+  name     = "terraform-remote-backend.piny940.com"
+  location = "US"
+
+  force_destroy               = false
+  public_access_prevention    = "enforced"
+  uniform_bucket_level_access = true
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "terraform-remote-backend.piny940.com"
+  }
+}
