dd9f7f6135977acff64fad53fa4baec4bf2f7e96
piny940 <83708535+piny940@users.noreply.github.com>
2024-09-09 15:14:21 +0900

    ディレクトリ構造を整備

--- gcp/production/main.tf
+++ gcp/production/main.tf
@@ -1,22 +1,4 @@
-provider "google" {
-  project = "prd-piny940"
-  default_labels = {
-    service     = "home-cluster"
-    environment = "production"
-  }
-}
-
-resource "google_storage_bucket" "terraform_remote_backend" {
-  name     = "terraform-remote-backend.piny940.com"
-  location = "US"
-
-  force_destroy               = false
-  public_access_prevention    = "enforced"
-  uniform_bucket_level_access = true
-}
-
-terraform {
-  backend "gcs" {
-    bucket = "terraform-remote-backend.piny940.com"
-  }
+module "clubroom" {
+  source = "../modules/clubroom"
+  env    = "production"
 }
--- gcp/staging/main.tf
+++ gcp/staging/main.tf
@@ -1,33 +1,4 @@
-provider "google" {
-  project = "stg-piny940"
-  default_labels = {
-    service     = "home-cluster"
-    environment = "staging"
-  }
-}
-
-resource "random_id" "terraform_remote_backend" {
-  byte_length = 8
-}
-
-resource "google_storage_bucket" "terraform_remote_backend" {
-  name     = "${random_id.terraform_remote_backend.hex}-terraform-remote-backend"
-  location = "US"
-
-  force_destroy               = false
-  public_access_prevention    = "enforced"
-  uniform_bucket_level_access = true
-}
-
-resource "local_file" "backend" {
-  file_permission = "0644"
-  filename        = "${path.module}/backend.tf"
-
-  content = <<-EOT
-  terraform {
-    backend "gcs" {
-      bucket = "${google_storage_bucket.terraform_remote_backend.name}"
-    }
-  }
-  EOT
+module "clubroom" {
+  source = "../../modules/clubroom"
+  env    = "staging"
 }
