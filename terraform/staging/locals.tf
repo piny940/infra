674aa815acec72a1c7cccdce265fc3a26e249cb1
piny940 <83708535+piny940@users.noreply.github.com>
2024-09-10 22:33:34 +0900

    workload identity pool idをlocalsで指定する

--- terraform/staging/locals.tf
+++ terraform/staging/locals.tf
@@ -3,4 +3,5 @@ locals {
   repo           = "piny940/infra"
   project_number = 645068243364
   project        = "stg-piny940"
+  workload_identity_pool_id = "default"
 }
--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -1,14 +1,9 @@
-data "google_client_config" "current" {}
-
-resource "google_iam_workload_identity_pool" "default" {
-  workload_identity_pool_id = "default"
-}
 module "terraform" {
   source                    = "../modules/terraform"
   env                       = local.env
   project                   = local.project
   project_number            = local.project_number
-  workload_identity_pool_id = google_iam_workload_identity_pool.default.workload_identity_pool_id
+  workload_identity_pool_id = local.workload_identity_pool_id
   repo                      = local.repo
 }
 module "clubroom" {
