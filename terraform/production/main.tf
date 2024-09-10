84c39fa3ff55e07583e3be8c0c505f6edcce03ff
piny940 <83708535+piny940@users.noreply.github.com>
2024-09-10 23:32:52 +0900

    terraform関連のmoduleをprdに追加

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -1,3 +1,11 @@
+module "terraform" {
+  source                    = "../modules/terraform"
+  env                       = local.env
+  project                   = local.project
+  project_number            = local.project_number
+  workload_identity_pool_id = local.workload_identity_pool_id
+  repo                      = local.repo
+}
 module "clubroom" {
   source = "../modules/clubroom"
   env    = "production"
