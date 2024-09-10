faf006e2d5bbdcdeadff96892c01610b7b7ec194
piny940 <83708535+piny940@users.noreply.github.com>
2024-09-10 22:39:26 +0900

    fmt

--- terraform/staging/locals.tf
+++ terraform/staging/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  env            = "staging"
-  repo           = "piny940/infra"
-  project_number = 645068243364
-  project        = "stg-piny940"
+  env                       = "staging"
+  repo                      = "piny940/infra"
+  project_number            = 645068243364
+  project                   = "stg-piny940"
   workload_identity_pool_id = "default"
 }
