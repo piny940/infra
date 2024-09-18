03130834a9f308ba94c0b285ead09d549909a43e
piny940 <83708535+piny940@users.noreply.github.com>
2024-09-18 10:22:31 +0900

    moduleをmainで使用

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -10,6 +10,13 @@ module "terraform" {
   workload_identity_pool_id = module.workload_identity_pool.workload_identity_pool_id
   repo                      = local.repo
 }
+module "kube_workload_identity" {
+  source                    = "../modules/kube_workload_identity"
+  env                       = local.env
+  project                   = local.project
+  project_number            = local.project_number
+  workload_identity_pool_id = module.workload_identity_pool.workload_identity_pool_id
+}
 module "clubroom" {
   source = "../modules/clubroom"
   env    = local.env
