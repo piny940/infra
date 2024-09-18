7eacbee1a166281f469135333e6b151769f1fd92
piny940 <83708535+piny940@users.noreply.github.com>
2024-09-18 10:19:30 +0900

    jwsk_jsonを保存するためのsecretを作成

--- terraform/modules/kube_workload_identity/main.tf
+++ terraform/modules/kube_workload_identity/main.tf
@@ -1,12 +1,23 @@
-resource "google_iam_workload_identity_pool_provider" "home_kubernetes" {
-  workload_identity_pool_id          = var.workload_identity_pool_id
-  workload_identity_pool_provider_id = "home-kubernetes"
-  display_name                       = "Home Kubernetes"
-  description                        = "for Kubernetes in the home"
-  attribute_mapping = {
-    "google.subject" = "assertion.sub"
-  }
-  oidc {
-    issuer_uri = "https://token.actions.githubusercontent.com"
+resource "google_secret_manager_secret" "home_kubernetes_cluster_jwks" {
+  secret_id = "home-kubernetes-cluster-jwks"
+  replication {
+    auto {}
   }
 }
+# data "google_secret_manager_secret_version" "home_kubernetes_cluster_jwks" {
+#   secret  = google_secret_manager_secret.home_kubernetes_cluster_jwks.secret_id
+#   version = "v1"
+# }
+# resource "google_iam_workload_identity_pool_provider" "home_kubernetes" {
+#   workload_identity_pool_id          = var.workload_identity_pool_id
+#   workload_identity_pool_provider_id = "home-kubernetes"
+#   display_name                       = "Home Kubernetes"
+#   description                        = "for Kubernetes in the home"
+#   attribute_mapping = {
+#     "google.subject" = "assertion.sub"
+#   }
+#   oidc {
+#     issuer_uri = "https://kubernetes.default.svc.cluster.local"
+#     jwks_json  = data.google_secret_manager_secret_version.home_kubernetes_cluster_jwks.secret_data["json"]
+#   }
+# }
