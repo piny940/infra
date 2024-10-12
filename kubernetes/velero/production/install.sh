5f3c447c7574cfa192d8cba6f088e3c9dfdadc97
piny940 <83708535+piny940@users.noreply.github.com>
2024-10-12 23:55:22 +0900

    fix

--- kubernetes/velero/production/install.sh
+++ kubernetes/velero/production/install.sh
@@ -1,3 +1,3 @@
-kustomize apply -k velero/production
+kubectl apply -k velero/production
 helm repo add vmware-tanzu https://vmware-tanzu.github.io/helm-charts
 helm install velero vmware-tanzu/velero --namespace velero --values velero/base/values.yaml --values velero/production/values.yaml --version 6.7.0
