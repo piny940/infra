205acb73513488ea4d44306d5b7bb79fe6190f7e
piny940 <83708535+piny940@users.noreply.github.com>
2024-10-15 20:13:41 +0900

    github actionsからawsにアクセスできるようにする

---
+++ aws/staging/github-action.tf
@@ -0,0 +1,41 @@
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = []
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "stg-github-actions-aws-owner"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Principal" : {
+          "Federated" : aws_iam_openid_connect_provider.github_actions.arn
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : [
+              "sts.amazonaws.com"
+            ]
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:piny940/infra:*"
+            ]
+          }
+        }
+      }
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "github_actions_lambda_full_access" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSLambda_FullAccess"
+}
+resource "aws_iam_role_policy_attachment" "github_actions_s3_full_access" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
