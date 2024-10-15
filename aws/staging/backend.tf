1ef06c1ebfe45f82f03974aa03810be3cc5016f4
piny940 <83708535+piny940@users.noreply.github.com>
2024-10-15 17:53:44 +0900

    bucket名を修正

--- aws/staging/backend.tf
+++ aws/staging/backend.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "terraform_remote_backend" {
-  bucket = "stg-terraform-remote-backend.piny940.com"
+  bucket = "stg-aws-tf-backend.piny940.com"
 }
 terraform {
   backend "s3" {
-    bucket = "stg-terraform-remote-backend.piny940.com"
+    bucket = "stg-aws-tf-backend.piny940.com"
     region = "ap-northeast-1"
     key    = "staging.tfstate"
   }
