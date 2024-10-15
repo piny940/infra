537b4cb553b37aa0c7f7c68156653f2bd623095b
piny940 <83708535+piny940@users.noreply.github.com>
2024-10-15 22:09:12 +0900

    github actionsのworkload identityをIaC管理する

---
+++ gcp/modules/terraform/main.tf
@@ -0,0 +1,44 @@
+resource "google_service_account" "terraform_github_actions" {
+  account_id                   = "terraform-github-actions"
+  display_name                 = "Terraform GitHub Actions"
+  create_ignore_already_exists = true
+}
+resource "google_project_iam_member" "terraform_github_actions_owner" {
+  project = var.project
+  member  = "serviceAccount:${google_service_account.terraform_github_actions.email}"
+  role    = "roles/owner"
+}
+resource "google_project_iam_member" "terraform_github_actions_workload_identity_user" {
+  project = var.project
+  member  = "serviceAccount:${google_service_account.terraform_github_actions.email}"
+  role    = "roles/iam.workloadIdentityUser"
+}
+
+resource "google_iam_workload_identity_pool_provider" "repo_github_actions" {
+  workload_identity_pool_id          = var.workload_identity_pool_id
+  workload_identity_pool_provider_id = "repo-github-actions"
+  display_name                       = "Terrform GitHub Actions"
+  description                        = "for Terraform GitHub Actions"
+  attribute_condition                = "assertion.repository == \"${var.repo}\""
+  attribute_mapping = {
+    "google.subject" = "assertion.sub"
+  }
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+resource "google_service_account_iam_member" "terraform_github_actions_env_workload_identity_user" {
+  service_account_id = google_service_account.terraform_github_actions.id
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principal://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${var.workload_identity_pool_id}/subject/repo:${var.repo}:environment:${var.env}"
+}
+resource "google_service_account_iam_member" "terraform_github_actions_pull_request_workload_identity_user" {
+  service_account_id = google_service_account.terraform_github_actions.id
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principal://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${var.workload_identity_pool_id}/subject/repo:${var.repo}:pull_request"
+}
+resource "google_service_account_iam_member" "terraform_github_actions_main_workload_identity_user" {
+  service_account_id = google_service_account.terraform_github_actions.id
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principal://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${var.workload_identity_pool_id}/subject/repo:${var.repo}:ref:refs/heads/main"
+}
