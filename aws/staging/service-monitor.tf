2327671993259190d424733af1189b8dc1903d7a
piny940 <83708535+piny940@users.noreply.github.com>
2024-10-15 23:38:48 +0900

    定期実行されるようにする

--- aws/staging/service-monitor.tf
+++ aws/staging/service-monitor.tf
@@ -24,6 +24,7 @@ data "archive_file" "dummy" {
 }
 resource "aws_lambda_function" "service-monitor" {
   function_name = "stg-service-monitor"
+  description   = "Service monitor of staging home cluster"
   role          = aws_iam_role.service-monitor.arn
   handler       = "index.handler"
   runtime       = "python3.12"
@@ -36,5 +37,55 @@ resource "aws_lambda_function" "service-monitor" {
       SLACK_API_TOKEN  = ""
       SLACK_CHANNEL    = local.slack_channel
     }
+  }
+}
+resource "aws_iam_role" "event_bridge" {
+  name = "stg-event-bridge"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "sts:AssumeRole",
+        "Principal" : {
+          "Service" : "scheduler.amazonaws.com"
+        },
+        "Sid" : "",
+        "Effect" : "Allow"
+      }
+    ]
+  })
+}
+
+# Event Bridge
+resource "aws_iam_role_policy" "event_bridge" {
+  name   = "stg-service-monitor-event-bridge"
+  role   = aws_iam_role.event_bridge.name
+  policy = data.aws_iam_policy_document.event_bridge.json
+}
+data "aws_iam_policy_document" "event_bridge" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "lambda:InvokeFunction",
+    ]
+
+    resources = [
+      aws_lambda_function.test_lambda.arn,
+    ]
+  }
+}
+resource "aws_scheduler_schedule" "service-monitor" {
+  name                         = "stg-service-monitor"
+  schedule_expression          = "cron(0/5 * * * ? *)"
+  schedule_expression_timezone = "Asia/Tokyo"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  target {
+    arn      = aws_lambda_function.service-monitor.arn
+    role_arn = aws_iam_role.event_bridge.arn
   }
 }
