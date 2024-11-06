791437600022a0bdef2cec90aa4f85004549763a
piny940 <83708535+piny940@users.noreply.github.com>
2024-11-06 18:31:18 +0900

    double quateをescape

--- kubernetes/apps/elastic-system/base/scripts/deletion-ilm.sh
+++ kubernetes/apps/elastic-system/base/scripts/deletion-ilm.sh
@@ -1,24 +1,24 @@
 # create ilm policy
-curl -X PUT -k -u "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" "https://${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/_ilm/policy/logs_auto_delete" -H 'Content-Type: application/json' -d'
+curl -X PUT -k -u "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" "https://${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/_ilm/policy/logs_auto_delete" -H 'Content-Type: application/json' -d"
 {
-  "policy": {
-    "phases": {
-      "delete": {
-        "min_age": ${LOGS_TTL},
-        "actions": {
-          "delete": {}
+  \"policy\": {
+    \"phases\": {
+      \"delete\": {
+        \"min_age\": ${LOGS_TTL},
+        \"actions\": {
+          \"delete\": {}
         }
       }
     }
   }
-}'
+}"
 
-curl -X PUT -k -u "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" "https://${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/_index_template/logstash_template" -H 'Content-Type: application/json' -d'
+curl -X PUT -k -u "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" "https://${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/_index_template/logstash_template" -H 'Content-Type: application/json' -d"
 {
-  "index_patterns": ["logstash-*"],
-  "template": {
-    "settings": {
-      "index.lifecycle.name": "logs_auto_delete"
+  \"index_patterns\": [\"logstash-*\"],
+  \"template\": {
+    \"settings\": {
+      \"index.lifecycle.name\": \"logs_auto_delete\"
     }
   }
-}
+}"
