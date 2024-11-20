c447b5de1ab93003efd95bc40811862e630ee2b6
piny940 <83708535+piny940@users.noreply.github.com>
2024-11-20 17:31:13 +0900

    app作成を簡単にできるようにする

---
+++ kubernetes/scripts/create-app.sh
@@ -0,0 +1,42 @@
+mkdir -p apps/$1
+mkdir -p apps/$1/base
+mkdir -p apps/$1/staging
+mkdir -p apps/$1/production
+
+cat <<EOF > apps/$1/base/kustomization.yaml
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources: []
+configMapGenerator:
+  - name: $1-conf
+    literals: []
+EOF
+
+cat <<EOF > apps/$1/production/kustomization.yaml
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+namespace: $1
+resources:
+  - ../base
+configMapGenerator:
+  - name: $1-conf
+    behavior: merge
+    literals: []
+EOF
+
+cat <<EOF > apps/$1/staging/kustomization.yaml
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+namespace: $1
+namePrefix: stg-
+resources:
+  - ../base
+configMapGenerator:
+  - name: $1-conf
+    behavior: merge
+    literals: []
+EOF
+
+sh scripts/flux-kustomize.sh
+
+echo "App $1 created!"
