8ef13ccefb888bfdbe933bc813587a24ab9893bd
piny940 <83708535+piny940@users.noreply.github.com>
2024-11-20 18:56:32 +0900

    cluster-secretを自動で作成するシェルスクリプトを作成

---
+++ kubernetes/scripts/cluster-tls.sh
@@ -0,0 +1,32 @@
+external_secret() {
+  app=$1
+  echo "apiVersion: external-secrets.io/v1beta1
+kind: ExternalSecret
+metadata:
+  name: cluster-tls
+spec:
+  secretStoreRef:
+    name: vault-secret-store
+    kind: ClusterSecretStore
+  refreshInterval: 10m
+  data:
+    - secretKey: tls.crt
+      remoteRef:
+        key: cluster-tls
+        property: tls.crt
+    - secretKey: tls.key
+      remoteRef:
+        key: cluster-tls
+        property: tls.key
+" > certificates/${app}.yaml
+}
+
+namespaces=$(cat namespaces/namespaces.yaml| grep -A 2 "kind: Namespace" - | grep "name:" | awk '{print $2}')
+for namespace in $namespaces; do
+  if [ ! -e "certificates/$namespace.yaml" ]; then
+    external_secret $namespace
+  fi
+  if ! yq '.resources[]' certificates/kustomization.yaml | grep -q $namespace; then
+    yq -i ".resources += [\"$namespace.yaml\"]" certificates/kustomization.yaml
+  fi
+done
