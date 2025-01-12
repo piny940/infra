c152faef24450570b790324fa02259eb79486e0c
piny940 <83708535+piny940@users.noreply.github.com>
2025-01-12 21:47:39 +0900

    datreeのCRD catalogをschemaに含める

--- kubernetes/scripts/kubeconform.sh
+++ kubernetes/scripts/kubeconform.sh
@@ -12,6 +12,7 @@ done
 kubeconform -summary \
   -schema-location default \
   -schema-location 'https://raw.githubusercontent.com/piny940/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json' \
+  -schema-location 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json' \
   -schema-location "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/{{.NormalizedKubernetesVersion}}/{{.ResourceKind}}.json" \
   -output json \
   -strict \
