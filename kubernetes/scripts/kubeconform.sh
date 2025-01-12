0b57edc7590e274d9a5a47bf2d3064fe4cbce6a2
piny940 <83708535+piny940@users.noreply.github.com>
2025-01-12 15:23:46 +0900

    kubeconformをCIに追加

---
+++ kubernetes/scripts/kubeconform.sh
@@ -0,0 +1,19 @@
+# build manifests for all apps
+for env in production staging
+do
+  for app in $(ls apps/)
+  do
+    mkdir -p tmp/kubeconform/manifests/"$app"
+    kustomize build apps/"$app"/"$env"/ > tmp/kubeconform/manifests/"$app"/"$env".yaml
+  done
+done
+
+# CRD Schemaが見つからない場合は追加する：https://github.com/piny940/CRDs-catalog
+kubeconform -summary \
+  -schema-location default \
+  -schema-location 'https://raw.githubusercontent.com/piny940/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json' \
+  -schema-location "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/{{.NormalizedKubernetesVersion}}/{{.ResourceKind}}.json" \
+  -output json \
+  -strict \
+  -ignore-missing-schemas \
+  tmp/kubeconform/manifests/
