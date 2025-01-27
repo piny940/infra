aae9f86638a38a27caf3af3e259b1f5b7532639c
piny940 <83708535+piny940@users.noreply.github.com>
2025-01-27 12:02:34 +0900

    init_kibanaにinit scriptsをまとめる

--- kubernetes/apps/elastic-system/base/scripts/init_kibana.sh
+++ kubernetes/apps/elastic-system/base/scripts/init_kibana.sh
@@ -1,4 +1,4 @@
-# create ilm policy
+# ログの自動削除
 curl -X PUT -k -u "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" "https://${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/_ilm/policy/logs_auto_delete" -H 'Content-Type: application/json' -d"
 {
   \"policy\": {
@@ -12,7 +12,6 @@ curl -X PUT -k -u "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" "https:/
     }
   }
 }"
-
 curl -X PUT -k -u "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" "https://${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/_index_template/logstash_template" -H 'Content-Type: application/json' -d"
 {
   \"index_patterns\": [\"fluentd.*\"],
@@ -22,3 +21,18 @@ curl -X PUT -k -u "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" "https:/
     }
   }
 }"
+
+# logのフォーマットをjsonに変更
+curl -X PUT -k -u "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" "https://${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/_index_template/log_format_json" -H 'Content-Type: application/json' -d"
+{
+  \"index_patterns\": [\"fluentd.*\"],
+  \"template\": {
+    \"mappings\": {
+      \"properties\": {
+        \"log\": {
+          \"type\": \"object\"
+        }
+      }
+    }
+  }
+}
