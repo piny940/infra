028f53c8b953076dbc6c27a9f80d2e94799af067
piny940 <83708535+piny940@users.noreply.github.com>
2025-03-17 22:57:26 +0900

    values.yamlをapps/veleroに移動
    Signed-off-by: piny940 <83708535+piny940@users.noreply.github.com>

--- kubernetes/velero/staging/install.sh
+++
@@ -1,2 +0,0 @@
-helm repo add vmware-tanzu https://vmware-tanzu.github.io/helm-charts
-helm install velero vmware-tanzu/velero --namespace velero --values velero/base/values.yaml --values velero/staging/values.yaml --version 8.1.0
