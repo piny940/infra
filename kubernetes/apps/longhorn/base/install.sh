ee1f09aa45e353b46028df8f5d0eab9a3836f027
piny940 <83708535+piny940@users.noreply.github.com>
2025-03-17 20:42:38 +0900

    longhornのinstall scriptをapps/longhorn以下に配置
    Signed-off-by: piny940 <83708535+piny940@users.noreply.github.com>

---
+++ kubernetes/apps/longhorn/base/install.sh
@@ -0,0 +1,11 @@
+if [ -z "$env" ]; then
+  echo "env is not set"
+  exit 1
+fi
+version=$(yq eval .spec.chart.spec.version apps/longhorn/base/helm.yaml)
+
+echo env = "$env"
+echo version = "$version"
+
+helm repo add longhorn https://charts.longhorn.io
+helm install longhorn longhorn/longhorn --namespace longhorn-system --values apps/longhorn/"$env"/values.yaml --version "$version
--- kubernetes/longhorn/install.sh
+++
@@ -1,2 +0,0 @@
-helm repo add longhorn https://charts.longhorn.io
-helm install longhorn longhorn/longhorn --namespace longhorn-system --values longhorn/values.yaml --version 1.8.1
